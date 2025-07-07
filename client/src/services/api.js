97906700f83a48d1cd101ee9284631b6b03e2d28
Ali Baran Demir <alibarandemir798@gmail.com>
2025-07-07 14:58:14 +0300

    api url added

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Axios instance oluşturma
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: API_URL + '/api',
     timeout: 10000, // 10 saniye timeout
     headers: {
         'Content-Type': 'application/json'
